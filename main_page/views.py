0d8bee9289fce971127645c132397a1be1372cb3
folegf76 <folegf76@gmail.com>
2023-01-11 18:29:29 +0200

    connected db cars and tariffs

--- main_page/views.py
+++ main_page/views.py
@@ -4,5 +4,8 @@ from .models import Cars, CarsCategory
 # Create your views here.
 def main_page(request):
     categories = CarsCategory.objects.filter(is_visible=True)
-    car = Cars.objects.filter(is_visible=True)
-    return render(request, 'index.html')
+    cars = Cars.objects.filter(is_visible=True)
+    return render(request, 'main_page.html', context={
+        'categories': categories,
+        'cars': cars
+    })
