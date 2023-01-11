ff909999d8c4c196980987006077e3225494e3ac
folegf76 <folegf76@gmail.com>
2023-01-11 11:27:45 +0200

    connected HTML and static

--- main_page/views.py
+++ main_page/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Cars, CarsCategory
 
 # Create your views here.
+def main_page(request):
+    categories = CarsCategory.objects.filter(is_visible=True)
+    car = Cars.objects.filter(is_visible=True)
+    return render(request, 'index.html')
