783c0c857684e96f2d52970d81d049154a943026
folegf76 <folegf76@gmail.com>
2023-02-06 17:59:41 +0200

    admin

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import CarsCategory, Cars, AboutUs, Comments, UserReservation
 
+
 # Register your models here.
+class CarsInline(admin.TabularInline):
+    model = Cars
+    raw_id_fields = ['category']
+
+
 admin.site.register(CarsCategory)
 admin.site.register(Cars)
 admin.site.register(AboutUs)
