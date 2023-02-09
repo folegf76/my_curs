0c282126428620285de1328aa0f88a5bb655bea3
folegf76 <folegf76@gmail.com>
2023-02-09 13:37:02 +0200

    adjusted admin settings

--- main_page/admin.py
+++ main_page/admin.py
@@ -2,14 +2,38 @@ from django.contrib import admin
 from .models import CarsCategory, Cars, AboutUs, Comments, UserReservation
 
 
-# Register your models here.
-class CarsInline(admin.TabularInline):
-    model = Cars
-    raw_id_fields = ['category']
+@admin.register(CarsCategory)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'position', 'is_visible', 'price', 'desc']
+    list_filter = ['is_visible', 'price', 'position']
+    list_editable = ['price', 'is_visible']
 
 
-admin.site.register(CarsCategory)
-admin.site.register(Cars)
-admin.site.register(AboutUs)
-admin.site.register(Comments)
-admin.site.register(UserReservation)
+@admin.register(Cars)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'position', 'is_visible', 'photo', 'desc', 'category']
+    list_filter = ['is_visible', 'position']
+    list_editable = ['is_visible']
+
+
+@admin.register(UserReservation)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone', 'email_us', 'date', 'manager_data_processed', 'is_processed']
+    list_filter = ['is_processed', 'phone', 'manager_data_processed']
+    list_editable = ['is_processed']
+
+
+@admin.register(AboutUs)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'position', 'is_visible', 'desc']
+    list_filter = ['is_visible', 'name']
+    list_editable = ['is_visible']
+
+
+@admin.register(Comments)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'position', 'is_visible', 'desc', 'photo']
+    list_filter = ['is_visible', 'name']
+    list_editable = ['is_visible']
+
+a
